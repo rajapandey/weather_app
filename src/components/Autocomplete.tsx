7e01406fe9783fd7f5fc065ce0700e9df3f88b83
Raja <raja.thinkjs.co>
2024-07-03 21:16:38 +0530

    Error handling

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -37,28 +37,35 @@ const Autocomplete: React.FC<AutocompleteProps> = ({ onSelectCity }) => {
   };
 
   return (
-    <div className="relative mb-4">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        placeholder="Search for a city"
-      />
-      {suggestions.length > 0 && (
-        <ul className="absolute left-0 right-0 mt-1 bg-white border rounded-md shadow-lg max-h-60 overflow-y-auto z-10">
-          {suggestions.map((suggestion, index) => (
-            <li
-              key={index}
-              onClick={() => handleSelectCity(suggestion)}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-            >
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="relative mb-4">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Search for a city"
+        />
+        {suggestions.length > 0 && (
+          <ul className="absolute left-0 right-0 mt-1 bg-white border rounded-md shadow-lg max-h-60 overflow-y-auto z-10">
+            {suggestions.map((suggestion, index) => (
+              <li
+                key={index}
+                onClick={() => handleSelectCity(suggestion)}
+                className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      {/* Display Error */}
+      <div className=" text-red-500 bold font-semibold">
+        {(inputValue && suggestions.length == 0) && "No city Found"}
+      </div>
+    </>
   );
 };
 
